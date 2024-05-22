dd4fbb1562c38eaff87a82d6ab61b39495a70842
Thanonphat.su (DKKs) <thanonphat.supho@krungsri.com>
2024-05-22 15:27:46 +0700

    [Add] base url link to repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/find-point-a4-coordinate-system-percent",
   server: {
     port: 5454
   }
