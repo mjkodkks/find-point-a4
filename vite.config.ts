56a07dc6bcbcda47a5038c6581d518501e9cbaab
Thanonphat.su (DKKs) <thanonphat.supho@krungsri.com>
2026-01-09 12:45:59 +0700

    chore: update dependencies and configuration, add vuedevtool

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import process from 'node:process'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig, loadEnv } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [vue()],
+    plugins: [vue(), vueDevTools()],
     base: env.BASE_URL,
     server: {
       port: 5454,
